0b9ba2f06b09f572a3094f2ef01fc5643986b2b3
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-05 00:47:05 +0100

    Add: CSS AOS Library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { Button } from "@/components/ui/button"
+import {useEffect} from react;
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+
 import 'boxicons/css/boxicons.min.css'
 
 export default function App() {
+  useEffect(() => {
+		Aos.init(
+      {
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        mirror: true, //allow animations when scrolling past elements
+        once: true, //animate only once while scrolling down
+        
+      }
+    );
+	})
+
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+    <h1 className="text-purple-600">Hello World from @typicaldeveloper</h1>
+    </>
   )
 }
 
