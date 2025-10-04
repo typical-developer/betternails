0cf59cda22e63ef4ae424a6a54d2d4e4901f2085
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-05 00:32:44 +0100

    Add: Shadcn UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import { Button } from "@/components/ui/button"
 
-function App() {
-
+export default function App() {
   return (
-    <>
-    <h1 className="text-red-400">test tailwindcss</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
-export default App
