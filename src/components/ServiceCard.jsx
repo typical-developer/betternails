6c232f24518c055be3a86a56aef5e051d2e44783
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-11 00:53:13 +0100

    Add: proper font weight

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -13,7 +13,7 @@ export default function ServiceCard(
             "px-6 py-8 shadow-md rounded-sm bg-white transition-shadow duration-150 ease-out hover:shadow-pink-400 flex-between gap-6",
             className
             )}>
-            <img src={`./img/${imgSrc}.jpg`} alt="" className="w-[100px] h-[100px] rounded-full" />
+            <img src={imgSrc} alt="" className="w-[100px] h-[100px] rounded-full" />
 
             <div className="space-y-2">
                 <h4 className="text-lg font-semibold">{serviceName}</h4>
