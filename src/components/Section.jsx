f47e726c7efdeee19d9ab8e264887cea1da9384c
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-11 01:45:47 +0100

    Add:  nesting SectionHeader

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,19 @@
 import cn from "../utils/cn";
 import Container from "./Container"; 
+import SectionHeader from "./SectionHeader";
 
-export default function Section({sectionId,className,containerClassName,children}) {
+export default function Section({sectionId,className,containerClassName,sectionHeaderClassName,sectionTitle,children}) {
     return(
         <>
         <section id={sectionId} className={cn(
             'relative min-h-[100svh]',
             className
         )}>
+
+            <SectionHeader label={sectionTitle} className={cn(
+                sectionHeaderClassName
+            )} />
+             
             <Container className={cn(
                 containerClassName)}>
                 {children}
