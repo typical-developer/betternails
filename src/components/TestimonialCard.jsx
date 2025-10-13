eccb4d37ad5673ae8b6fda9f85e96a5d4342a52d
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-13 17:03:24 +0100

    Add: Testimoinal section done

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -2,19 +2,32 @@ import cn from "../utils/cn";
 
 export default function TestimonialCard({
     className,
-    clientName = "John Doe",
+    clientName = "Jane Doe",
     username = "@johndoe",
     quote = "The is a long quote where i am meant to glaze the hell outta betty's nails as if the did threaten to slime me if I didn't",
     imgSrc,
+    imgAlt = "jane Doe",
 }){
     return(
         <>
             <div className={cn(
-                "px-6 py-8 shadow-md rounded-sm bg-white transition-shadow duration-150 ease-out hover:shadow-pink-400 flex-between gap-6",
+                "relative flex flex-col px-8 py-10 shadow-md rounded-sm bg-white transition-shadow duration-150 ease-out border-b-4 border-b-pink-300  gap-4",
                 className
             )}>
-                {clientName}
-                {quote}
+
+
+                <div className="flex items-center gap-6">
+                    <img src={imgSrc} alt={imgAlt} className="w-[60px] h-[60px] rounded-full aspect-square"/>
+
+                    <div className="flex flex-col">
+                        <p className="font-semibold text-base">{clientName}</p>
+                        <p className="font-normal text-sm text-gray-800">{username}</p>
+                    </div>
+                </div>
+                
+                <div>
+                    <p className="text-sm">"<i>{quote}</i>"</p>
+                </div>
             </div>
         </>
     );
