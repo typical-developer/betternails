70ff4051f6836bdf168ddd45268bcdaf06212342
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-13 11:17:44 +0100

    Add: Imported images

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,44 +1,50 @@
 import Section from "../components/Section";
-import SectionHeader from "../components/SectionHeader";
 import ServiceCard from "../components/ServiceCard";
-    const numberOfCards = [1,2,3,4,5,6]
-export default function Services() {
 
+//services images
+import acrylicNails from "../assets/service-img/acrylic-nails.jpg"
+import gelPolish from "../assets/service-img/gel-polish-manicure.jpg"
+import nailArt from "../assets/service-img/nail-art.jpg"
+import nailRepairs from "../assets/service-img/nail-repairs.jpg"
+import pedicures from "../assets/service-img/pedicures.jpg"
+import crystalNailArt from "../assets/service-img/crystal-nail-art.jpg"
+
+export default function Services() {
     const services = [
         {id: 1,
         serviceName: "Acrylic Nail Set",
         desc: "Full set of durable, customizable acrylic nails to achieve your desired length and shape.",
-        imgSrc: './img/acrylic-nails.jpg',
+        imgSrc: acrylicNails,
         price: 4500},
 
         {id: 2,
         serviceName: "Gel Polish Maicures",
         desc: "Long-lasting gel polish application with proper nail prep for maximum durability and shine.",
-        imgSrc: './img/gel-polish-manicure.jpg',
+        imgSrc: gelPolish,
         price: 5000},
 
         {id: 3,
         serviceName: "Nail Art",
         desc: "Custom designs, from simple accents to elaborate artwork, tailored to your preferences.",
-        imgSrc: './img/nail-art.jpg',
+        imgSrc: nailArt,
         price: 2500},
 
         {id: 4,
         serviceName: "Nail Repairs",
         desc: "Quick and effective repairs for broken or damaged nails to restore their appearance.",
-        imgSrc: './img/nail-repairs.jpg',
+        imgSrc: nailRepairs,
         price: 2000},
 
         {id: 5,
         serviceName: "Pedicures",
         desc: "Relaxing foot treatment with exfoliation, massage, and polish application.",
-        imgSrc: './img/pedicures.jpg',
+        imgSrc: pedicures,
         price: 2500},
 
         {id: 6,
         serviceName: "Crystal & 3D Nail Art",
         desc: "Premium nail art featuring crystals, charms, and 3D elements for special occasions.",
-        imgSrc: './img/crystal-nail-art.jpg',
+        imgSrc: crystalNailArt,
         price: 3500},
     ]
 
