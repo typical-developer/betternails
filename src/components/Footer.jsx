58e5198c010b5cbee2d6556af07cbafa4c009a7f
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-13 15:58:17 +0100

    Add: Pinterest attribution

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
                 <p className="text-base text-white">&copy; 2021 - 2025 Nails By Betty | All rights reserved</p>
 
                 <div className="flex-center">
-                    <span className="text-xs text-gray-100 max-w-xl text-center">Some images on this site are sourced from <a href="https://freepik.com" rel="noopener noreferrer" className="underline hover:no-underline">Freepik</a>. All rights to these images remain with their respective creators Made by <a href="https://github.com/AwaniCaleb/" rel="noopener noreferrer" className="underline hover:no-underline">Awani Caleb</a> & <a href="https://github.com/typical-developer/" rel="noopener noreferrer" className="underline hover:no-underline">Chris Nzenwata</a> with 🧡 and ☕</span> </div>
+                    <span className="text-xs text-gray-100 max-w-xl text-center">Some images on this site are sourced from <a href="https://freepik.com" rel="noopener noreferrer" className="underline hover:no-underline">Freepik</a> and <a href="https://pinterest.com" rel="noopener noreferrer" className="underline hover:no-underline">Pinterest</a>. All rights to these images remain with their respective creators Made by <a href="https://github.com/AwaniCaleb/" rel="noopener noreferrer" className="underline hover:no-underline">Awani Caleb</a> & <a href="https://github.com/typical-developer/" rel="noopener noreferrer" className="underline hover:no-underline">Chris Nzenwata</a> with 🧡 and ☕</span> </div>
             </Container>
         </footer>
         </>
