074ab620c32c2032745e1bafde0187cac7fb966b
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-20 15:34:50 +0100

    feat: Contact form serviced select and date and time sheduling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,21 @@
+import { servicesList } from "../utils/servicesList"
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Calendar24 } from "./Calendar24"
 
 export default function ContactForm() {
+
   return (
     <Card className="max-w-[1200px] w-full lg:w-[400px] mx-auto py-10 shadow-lg">
       <CardHeader>
@@ -18,8 +30,24 @@ export default function ContactForm() {
         >
           <Input type="text" name="name" placeholder="Your Name" required />
           <Input type="email" name="email" placeholder="Your Email" required />
-          <Textarea name="message" placeholder="Your Message" required />
-
+          <Textarea name="message" placeholder="Your Message" />
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a service" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Services</SelectLabel>
+                {servicesList.map(({id,serviceName}) => 
+                  <SelectItem 
+                  key={id} value={serviceName}>
+                    {serviceName}
+                  </SelectItem>
+                )}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Calendar24 className="w-full" />
           <CardFooter className="p-0 mt-2">
             <Button type="submit" className="w-full">Send Message</Button>
           </CardFooter>
