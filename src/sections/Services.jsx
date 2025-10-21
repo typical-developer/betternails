33014f1fabad85716b5978f7fadb37f05a375545
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-10-21 11:17:32 +0100

    feat: services all imported from servcicesList.js

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,61 +1,16 @@
 import Section from "../components/Section";
 import ServiceCard from "../components/ServiceCard";
 
-//services images
-import acrylicNails from "../assets/service-img/acrylic-nails.jpg"
-import gelPolish from "../assets/service-img/gel-polish-manicure.jpg"
-import nailArt from "../assets/service-img/nail-art.jpg"
-import nailRepairs from "../assets/service-img/nail-repairs.jpg"
-import pedicures from "../assets/service-img/pedicures.jpg"
-import crystalNailArt from "../assets/service-img/crystal-nail-art.jpg"
-
 import { servicesList } from "../utils/servicesList";
 
 export default function Services() {
-    const services = [
-        {id: 1,
-        serviceName: "Acrylic Nail Set",
-        desc: "Full set of durable, customizable acrylic nails to achieve your desired length and shape.",
-        imgSrc: acrylicNails,
-        price: 4500},
-
-        {id: 2,
-        serviceName: "Gel Polish Maicures",
-        desc: "Long-lasting gel polish application with proper nail prep for maximum durability and shine.",
-        imgSrc: gelPolish,
-        price: 5000},
-
-        {id: 3,
-        serviceName: "Nail Art",
-        desc: "Custom designs, from simple accents to elaborate artwork, tailored to your preferences.",
-        imgSrc: nailArt,
-        price: 2500},
-
-        {id: 4,
-        serviceName: "Nail Repairs",
-        desc: "Quick and effective repairs for broken or damaged nails to restore their appearance.",
-        imgSrc: nailRepairs,
-        price: 2000},
-
-        {id: 5,
-        serviceName: "Pedicures",
-        desc: "Relaxing foot treatment with exfoliation, massage, and polish application.",
-        imgSrc: pedicures,
-        price: 2500},
-
-        {id: 6,
-        serviceName: "Crystal & 3D Nail Art",
-        desc: "Premium nail art featuring crystals, charms, and 3D elements for special occasions.",
-        imgSrc: crystalNailArt,
-        price: 3500},
-    ]
 
     return(
         <>
         <Section className="bg-white" containerClassName="lg:flex-center lg:min-h-[100svh]" sectionId="services" title={true} sectionTitle="Our Services">
 
                 <div className="mt-10 lg:mt-0 max-w-[1400px] grid md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-                    {services.map(({id,serviceName,desc,imgSrc,price},delay = 0) => 
+                    {servicesList.map(({id,serviceName,desc,imgSrc,price},delay = 0) => 
                         <ServiceCard 
                             data-aos="fade-up"
                             data-aos-delay={delay * 50}
--- src/utils/servicesList.js
+++ src/utils/servicesList.js
@@ -1,19 +1,46 @@
+  //services images
+  import acrylicNails from "../assets/service-img/acrylic-nails.jpg"
+  import gelPolish from "../assets/service-img/gel-polish-manicure.jpg"
+  import nailArt from "../assets/service-img/nail-art.jpg"
+  import nailRepairs from "../assets/service-img/nail-repairs.jpg"
+  import pedicures from "../assets/service-img/pedicures.jpg"
+  import crystalNailArt from "../assets/service-img/crystal-nail-art.jpg"
+  
+  
   export const servicesList = [
-    {id: 1,
-    serviceName: 'Acrylic Nail Set'},
+        {id: 1,
+        serviceName: "Acrylic Nail Set",
+        desc: "Full set of durable, customizable acrylic nails to achieve your desired length and shape.",
+        imgSrc: acrylicNails,
+        price: 4500},
 
-    {id: 2,
-    serviceName: 'Gel Polish Manicures'},
+        {id: 2,
+        serviceName: "Gel Polish Maicures",
+        desc: "Long-lasting gel polish application with proper nail prep for maximum durability and shine.",
+        imgSrc: gelPolish,
+        price: 5000},
 
-    {id: 3,
-    serviceName: 'Nail Art'},
+        {id: 3,
+        serviceName: "Nail Art",
+        desc: "Custom designs, from simple accents to elaborate artwork, tailored to your preferences.",
+        imgSrc: nailArt,
+        price: 2500},
 
-    {id: 4,
-    serviceName: 'Nail Repairs'},
+        {id: 4,
+        serviceName: "Nail Repairs",
+        desc: "Quick and effective repairs for broken or damaged nails to restore their appearance.",
+        imgSrc: nailRepairs,
+        price: 2000},
 
-    {id: 5,
-    serviceName: 'Pedicures'},
+        {id: 5,
+        serviceName: "Pedicures",
+        desc: "Relaxing foot treatment with exfoliation, massage, and polish application.",
+        imgSrc: pedicures,
+        price: 2500},
 
-    {id: 6,
-    serviceName: 'Crystal & 3D Nail Art'},
+        {id: 6,
+        serviceName: "Crystal & 3D Nail Art",
+        desc: "Premium nail art featuring crystals, charms, and 3D elements for special occasions.",
+        imgSrc: crystalNailArt,
+        price: 3500},
   ]
